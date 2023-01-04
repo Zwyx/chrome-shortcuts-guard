88cc6a88dbeb8e167ca72a20f745e2c769cab1ca
Zwyx <29386932+Zwyx@users.noreply.github.com>
2023-01-04 20:03:00 +0800

    Add exception for GitHub command palette shortcut

--- src/script.js
+++ src/script.js
@@ -1,6 +1,16 @@
 window.addEventListener(
 	"keydown",
 	(e) => {
+		// Exception for the GitHub command palette shortcut
+		if (
+			location.hostname === "github.com" &&
+			e.ctrlKey &&
+			e.altKey &&
+			e.code === "KeyK"
+		) {
+			return;
+		}
+
 		if (e.ctrlKey) {
 			e.stopPropagation();
 		}
